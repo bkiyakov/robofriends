3385d37efe271c93b916a9610212f4fd7e839542
Bjorn <capbarkley16@gmail.com>
2019-02-06 14:24:08 +0400

    react app4

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Card = ({ name, email, id}) => {
+const Card = ({ name, email, id, phone, city}) => {
 	return (
 		<div className='tc bg-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robophoto' src={`https://robohash.org/robot${id}?200x200`} />
+			<img alt='robophoto' src={`https://robohash.org/robot${id}?set=set2&200x200`} />
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
+				<p>{phone}</p>
+				<p>{city}</p>
 			</div>
 		</div>
 	);
--- src/CardList.js
+++ src/CardList.js
@@ -10,7 +10,9 @@ const CardList = ({robots}) => {
 						key={robots[i].id}
 						id={robots[i].id}
 						name={robots[i].name}
-						email={robots[i].email}/>
+						email={robots[i].email}
+						phone={robots[i].phone}
+						city={robots[i].address.city}/>
 					);
 				})
 			}
